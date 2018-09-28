70da97096b169fd65907b6039e75108f67902a52
beemolin <chienhsuan.lin@igroup.tw>
2018-09-28 16:57:38 +0800

    新增 確認按鈕

--- Picker.js
+++ Picker.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import {
     View,
+    Text,
+    TouchableOpacity
 } from 'react-native';
 
 import BaseDialog from './view/BaseDialog';
@@ -13,10 +15,16 @@ export default class Picker extends BaseDialog {
     static defaultProps = {
         items: [{key: 0, value: 'a'}, {key: 1, value: 'b'}, {key: 2, value: 'c'}],
         onPickerSelected: null,
+        confirmText: 'OK',
+        confirmBtnColor: '#fff',
+        confirmTextColor: '#333',
+        onPress: null,
     };
 
     constructor(props) {
         super(props);
+
+        this.select = this.props.items[0];
     }
 
     _getContentPosition() {
@@ -26,17 +34,31 @@ export default class Picker extends BaseDialog {
     renderContent() {
         return (
             <View style={{
-                width: this.mScreenWidth, flexDirection: 'row'
+                width: this.mScreenWidth, flexDirection: 'column'
             }}>
                 <PickerView
                     list={this.props.items}
                     onPickerSelected={(toValue) => {
-                        this.props.onPickerSelected && this.props.onPickerSelected(toValue);
-                        this.dismiss();
+                        this.select = toValue;
                     }}
                     fontSize={this.getSize(14)}
+                    itemSelectedColor={0x333333ff}
                     itemWidth={this.mScreenWidth}
                     itemHeight={this.getSize(40)} />
+                <View style={{width: this.mScreenWidth}}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            this.props.onPickerSelected && this.props.onPickerSelected(this.select);
+                            this.dismiss();
+                        }}
+                        style={{
+                            width: this.mScreenWidth, height: this.getSize(50),
+                            backgroundColor: this.props.confirmBtnColor,
+                            justifyContent: 'center', alignItems: 'center', borderTopWidth: 2
+                        }}>
+                        <Text style={{ color: this.props.confirmTextColor, fontSize: this.getSize(16), fontWeight: '400' }}>{this.props.confirmText}</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
